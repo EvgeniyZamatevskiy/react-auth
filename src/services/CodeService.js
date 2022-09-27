e378984240a57ea334bbc971c18077aec2145eef
EvgeniyZamatevskiy <jerry.zam.999@gmail.com>
2022-09-27 14:48:27 +0300

    feat: Add request to send code and verify phone number

---
+++ src/services/CodeService.js
@@ -0,0 +1,34 @@
+import {createApi, fetchBaseQuery} from "@reduxjs/toolkit/query/react"
+import {EMPTY_STRING} from "../const"
+
+export const authApi = createApi({
+  reducerPath: "authApi",
+  baseQuery: fetchBaseQuery({
+    baseUrl: "http://5.44.41.26:1488/",
+  }),
+  endpoints: (build) => ({
+    sendCode: build.mutation({
+      query: (phoneNumber) => {
+
+        const convertedPhoneNumber = phoneNumber.replace(/[^\d]/g, EMPTY_STRING)
+
+        return {
+          url: "user/auth/sms",
+          method: "POST",
+          body: {"phoneNumber": convertedPhoneNumber}
+        }
+      },
+    }),
+    phoneNumberVerification: build.mutation({
+      query: (data) => {
+        return {
+          url: `user/auth/sms/${data.code}`,
+          method: "POST",
+          body: {"token": data.token},
+        }
+      }
+    })
+  }),
+})
+
+export const {useSendCodeMutation, usePhoneNumberVerificationMutation} = authApi
