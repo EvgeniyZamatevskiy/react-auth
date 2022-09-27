c9ce3a61ffae33b933dd0fa6cfa594c0bd3149bb
EvgeniyZamatevskiy <jerry.zam.999@gmail.com>
2022-09-27 14:49:41 +0300

    feat: Add a new reducer and middleware

--- src/redux/index.js
+++ src/redux/index.js
@@ -1,8 +1,11 @@
 import {configureStore} from "@reduxjs/toolkit"
 import authSlice from "./auth/slice"
+import {authApi} from "../services/CodeService"
 
 export const store = configureStore({
   reducer: {
     auth: authSlice,
+    [authApi.reducerPath]: authApi.reducer,
   },
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(authApi.middleware),
 })
